c4c67610ac20e5bf0abbd580a1200e8ea91ff8ce
gbrogni <harden13brogni@gmail.com>
2024-05-14 10:47:16 -0300

    fix: format currency

--- frontend/src/utils/formatToCurrency.ts
+++ frontend/src/utils/formatToCurrency.ts
@@ -1,11 +1,8 @@
-export function formatToCurrency(valueInCents: number) {
-    const valueInReal = valueInCents / 100;
-    const formattedNumber = new Intl.NumberFormat('pt-BR', {
-      style: 'currency',
-      currency: 'BRL',
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
-    }).format(valueInReal);
-  
-    return formattedNumber;
-  }
+export function formatToCurrency(value: number) {
+  const formattedNumber = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
+
+  return formattedNumber;
+}
